a1a380b610d99d3994d319fb583d866cd102e243
Jhordany Polanco <74990160+JhordP@users.noreply.github.com>
2021-05-05 16:19:20 -0400

    Se subió APP

---
+++ ContaCosas Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ContaCosas_Consola
+{
+    class Program
+    {
+        protected InterfazUsuario run = new InterfazUsuario();
+        static void Main(string[] args)
+        {
+            new Program().run.Ejecutar();   
+        }
+    }
+}
